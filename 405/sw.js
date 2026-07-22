518757f84f0ef4a5683d8abe2868396413d47fac
Gwenaël Bourgault <gwenael.bourgault@gmail.com>
2026-07-22 17:56:34 +0400

    Actualiser le cache d’aperçu

--- 405/sw.js
+++ 405/sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "405-dopamine-v2";
+const CACHE_NAME = "405-dopamine-v3";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -8,7 +8,7 @@ const APP_SHELL = [
   "./assets/styles.css?v=20260722-1",
   "./assets/content.js?v=20260722-1",
   "./assets/engine.js?v=20260722-1",
-  "./assets/app.js?v=20260722-2",
+  "./assets/app.js?v=20260722-3",
   "/assets/img/mathsgo-logo-390.png",
   "/assets/img/favicon-192.png",
   "/assets/img/apple-touch-icon.png"
